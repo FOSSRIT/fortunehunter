61d2bd391525c322c70662acdaab69bbb4e28547
Justin Lewis <jtl1728@rit.edu>
2010-01-14 13:32:41 -0500

    Fixed depreciated message in setup.py

--- MAFH.activity/setup.py
+++ MAFH.activity/setup.py
@@ -1,4 +1,4 @@
 from sugar.activity import bundlebuilder
 
 if __name__ == "__main__": ##checks whether the class is started as main; not absolutely necessary
-          bundlebuilder.start('MAFH')
+          bundlebuilder.start()
