c7bf4ec4cf932c98e6620f437119f6bad20f46ba
Justin Lewis <jtl1728@rit.edu>
2010-02-15 12:50:40 -0500

    Expanded constants file

--- MAFH.activity/constants.py
+++ MAFH.activity/constants.py
@@ -1,4 +1,5 @@
 import os.path
+from gettext import gettext as _
 
 BASE_PATH = os.path.dirname(__file__) + "/assets/"
 #BASE_PATH = "/home/liveuser/GIT_REPOS/MAFH/mainline/MAFH.activity/assets/"
@@ -11,3 +12,91 @@ PUZZLE_PATH = BASE_PATH + "image/puzzle/"
 FMC_PATH = BASE_PATH + "fmc/"
 TOUR_PATH = BASE_PATH + "/image/tutorial/"
 CHAR_PATH = BASE_PATH + "/image/character/"
+
+DOOR_ORDER = ['N','S','E','W']
+DOOR_INDEX = {
+    'N':_('North'),
+    'S':_('South'),
+    'E':_('East'),
+    'W':_('West')
+    }
+
+DOOR_FLAGS = {
+    '0':_('None'),
+    'u':_('Unlocked'),
+    'l':_('Locked'),
+    'p':_('Puzzle'),
+    'b':_('Locked Puzzle'),
+    'e':_('Entrance'),
+    'x':_('Exit')
+    }
+
+SPEC_FLAGS = {
+    '0':_('None'),
+    'M':_('Merchant'),
+    'P':_('Puzzle')
+    }
+
+ENEM_INDEX = {
+    '0':_('None'),
+    '1':_('Wizard Adept'),
+    '2':_('Goblin'),
+    '3':_('Orc'),
+    '4':_('Stone Golem'),
+    '5':_('Serratula'),
+    '6':_('Feren')
+    }
+
+ITEM_INDEX = {
+    '0':_('None'),
+    '1':_("Ancient Amulet"),
+    '2':_("Rusted Blade"),
+    '3':_("Great Sword"),
+    '4':_("Crescent Sword"),
+    '5':_("Cardinal"),
+    '6':_("Sun Moon"),
+    '7':_("Earth Vest"),
+    '8':_("Wind Breaker"),
+    '9':_("Flame Leggings"),
+    'a':_("Dark Cowl"),
+    'b':_("Celestial Armor"),
+    'c':_("Jewel Shard"),
+    'd':_("Broken Hourglass"),
+    'e':_("Radiant Vial"),
+    'f':_("Honor Tome"),
+    'g':_("Valor Tome"),
+    'h':_("Ruby"),
+    'i':_("Sapphire"),
+    'j':_("Emerald"),
+    'k':_("Diamond"),
+    'l':_("Remedy"),
+    'm':_("Elixir"),
+    'n':_("Panacea"),
+    'o':_("High Elixir"),
+    'p':_("Nostrum"),
+    'q':_("Small Key"),
+    'r':_("Big Key"),
+}
+
+ITEM_FLAGS = {
+    '0':_('None'),
+    'v':_('Visible'),
+    'h':_('Hidden'),
+    'b':_('Battle')
+}
+
+# Integer Driven
+THEME_NAME = [
+    _('Default Theme'),
+    _('Ice'),
+    _('Fire'),
+    _('Ice'),
+    _('Jungle'),
+    _('Desert'),
+    _('Astral')
+    ]
+
+NORTH=1
+SOUTH=3
+EAST=0
+WEST=2
