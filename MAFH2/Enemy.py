9933d35ca9f2fea7c25645b7272f203d78936e89
Jon Meschino <jam2707@rit.edu>
2010-02-18 01:52:41 -0500

    lol

--- MAFH2/Enemy.py
+++ MAFH2/Enemy.py
@@ -6,23 +6,23 @@ import pygame
 from Actor import Actor
 from AnimatedSprite import Spritesheet, AnimatedSprite
 ENEMY = {
-    '1':{'name':_('Wizard Adept'),'img':"concept_wizard.png",'hp':20,'att':2,'weak':'special', 'sprite':(1,1,181,365)},
-    '2':{'name':_('Goblin'),'img':"concept_goblin.png",'hp':40,'att':3,'weak':'fire','sprite':(1,1,217,317)},
-    '3':{'name':_('Orc'),'img':"concept_orc.png",'hp':50,'att':5,'weak':'lightning','sprite':(1,1,264,338)},
-    '4':{'name':_('Stone Golem'),'img':"concept_orc.png",'hp':10,'att':6,'weak':'missile','sprite':(1,1,264,338)},
+    '1':{'name':_('Wizard Adept'),'img':"anim_test.gif",'hp':20,'att':2,'weak':'special', 'sprite':(3,2, 156,166)}, #concept_wizard.png , (1,1, 181,365)
+    '2':{'name':_('Goblin'),'img':"dog~.png",'hp':40,'att':3,'weak':'fire','sprite':(2,1, 58/2,39)},                #concept_goblin.png , (1,1, 217,317)
+    '3':{'name':_('Orc'),'img':"concept_orc.png",'hp':50,'att':5,'weak':'lightning','sprite':(1,1, 264,338)},
+    '4':{'name':_('Stone Golem'),'img':"concept_orc.png",'hp':10,'att':6,'weak':'missile','sprite':(1,1, 264,338)},
     '5':{'name':_('Serratula'),'img':"Crab.png",'hp':125,'att':12,'weak':'crit','sprite':(1,1,400,400)},
     '6':{'name':_('Feren'),'img':"faren.png",'hp':1500,'att':10,'weak':'special','sprite':(1,1,250,250)},
     '7':{'name':_('Cave Crab'),'img':"Crab.png",'hp':50,'att':7,'weak':'missile','sprite':(1,1,400,400)},
     '8':{'name':_('Frost Giant'),'img':"frost_giant.png",'hp':45,'att':9,'weak':'fire','sprite':(1,1,250,250)},
     #UPDATE BONESPROUT VALUES
     '9':{'name':_('Bonesprout'),'img':"bonesprout.png",'hp':45,'att':9,'weak':'fire','sprite':(1,1,250,250)},
-    'a':{'name':_('anim_test'),'img':"anim_test.gif",'hp':45,'att':9,'weak':'fire','sprite':(3,2, 156, 166)}
+    'a':{'name':_('anim_test'),'img':"anim_test.gif",'hp':45,'att':9,'weak':'fire','sprite':(3,2, 156,166)},
 }
 
 class Enemy(Actor):
     def __init__(self, id):
         Actor.__init__(self)
-        id = 'a'
+        #id = 'a'
         self.weakness=None
         self.eqItems_Ar = []    #equipped items
         self.attacks_Ar = []    #associated array for attack string names and attack power values
