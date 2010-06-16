3e8b92b813f30c1713785441b200541821bb7ba4
slm <slm@slm-laptop.(none)>
2010-06-16 14:08:44 -0400

    Pushing the initial state of the complete test which will be temporarily located in /anim/completeTest

---
+++ anim/completeTest/fileTypeTests.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+#import sys
+
+#= .image() Animation Test ========
+#- set up -------------------------
+
+def imgSetup():
+    print "imgSetup"
+
+def imgTest():
+    print "imgTest"
+
+#= .sprite() Animation Test =======
+#- set up -------------------------
+
+def spriteSetup():
+    print "spriteSetup"
+
+def spriteTest():
+    print "spriteTest"
+
+
+#= Scalability Test ===============
+#- set up -------------------------
+
+def scaleSetup():
+    print "scaleSetup"
+
+def scaleTest():
+    print "scaleTest"
+
+
+print "\n\nWelcome to the master File Type Tester Interface"
+print "Authors: Scott 'JT' Mengel and Dave Silverman"
+print "\nPlease select the test(s) you want to run in the order you want to run them."
+print "Enter 'Exit' into the prompt to close the program. (NOTE: not working yet)\n"
+print "1. Simple File Type Variety Test, as images (NOTE: not working yet)"
+print "2. Simple File Type variety Test, as sprites (NOTE: not working yet)"
+print "3. Selected Scalability Test (NOTE: not working yet)"
+file=open('./testresult.csv','a')
+list=[]
+keyIn="temp val"
+val=0
+while keyIn is not None:
+    keyIn=raw_input(">")
+    list[val]=int(keyIn)
+    val=val+1
+
+print list+" and switching now..."
+#
+#for i in list:
+#        call what's in list[i] somehow
+#    print i
+#    if i == keyInt 
+#    list[int(i)]
+#    switch.get( i , sys.exit("Goodbye!") )
+#    print 'looped through'
