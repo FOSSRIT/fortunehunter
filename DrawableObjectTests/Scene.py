d416d9769b051790bb6351130c4fffccec05360e
davesilver <nibor123@gmail.com>
2010-07-14 12:06:16 -0400

    Updated the Scene class to include shell definitions for all of its intended definitions and a partially completed initializer.

--- DrawableObjectTests/Scene.py
+++ DrawableObjectTests/Scene.py
@@ -2,4 +2,36 @@ import pygame
 
 class Scene:
 
-    def __init__(self, filename):
+    def __init__(self, drawableObjects):
+    
+       self._my_Objects = []
+
+       cnt = 0
+       while cnt < len(drawableObjects):
+           self._my_objects.append(drawableObjects[cnt],[drawableObjects[cnt].getXPos(),drawableObjects[cnt].getYPos()])
+    
+    def calcPosition(self):
+    
+    def calcSize(self):
+    
+    def addObjects(self):
+    
+    def setRelativePositions(self):
+    
+    def removeObject(self):
+    
+    def getObject(self):
+    
+    def moveObjects(self):
+    
+    def nudgeScene(self):
+    
+    def setPosition(self):
+    
+    def getPosition(self):
+    
+    def scaleObjects(self):
+    
+    def scaleScene(self):
+    
+    def updateAnimations(self):
