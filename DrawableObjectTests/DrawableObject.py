1570b27e62fc555c4b385dfaa060d04fa9aa74c5
davesilver <nibor123@gmail.com>
2010-07-19 14:36:59 -0400

    Updating Tests

--- DrawableObjectTests/DrawableObject.py
+++ DrawableObjectTests/DrawableObject.py
@@ -97,7 +97,7 @@ class DrawableObject(pygame.sprite.Sprite):
         self.myAngle += angle
         while  cnt < len(self._images):
 
-            self._images[cnt] = pygame.transform.rotate(self._images[cnt], angle)
+            self._images[cnt] = pygame.transform.rotate(self._origImages[cnt], self.myAngle)
             cnt += 1
 
     def getRotation(self):
