0854df3bce436a77acf9bd23206015800ecfc2ca
davesilver <nibor123@gmail.com>
2010-07-26 16:33:38 -0400

    Editing Sprite tests

--- devtools/CompleteTestKit/dirtyTest2.py
+++ devtools/CompleteTestKit/dirtyTest2.py
@@ -8,7 +8,7 @@ from DrawableObject import DrawableObject
 from DynamicDrawableObject import DynamicDrawableObject
 pygame.init()
 
-FRAME=800
+FRAME=1200
 screenWidth = 600
 screenHeight = 400
 numImages = 1
