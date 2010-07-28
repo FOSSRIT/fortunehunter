6c6361a7041e4113158deca3256f1f9858fc7866
davesilver <nibor123@gmail.com>
2010-07-28 13:14:23 -0400

    Testing Drawable Font Objects

--- devtools/CompleteTestKit/DrawableFontObject.py
+++ devtools/CompleteTestKit/DrawableFontObject.py
@@ -7,30 +7,9 @@ class DrawableFontObject(DrawableObject, pygame.sprite.Sprite):
 
         self.font = font
         self.textImage = font.render(text, True, (0,0,0))
+        self._image = self.textImage
         DrawableObject.__init__(self, [self.textImage], '', fps, x, y, xVelocity, yVelocity)
 
     def changeText(self, newtext):
 
         self._images[0] = font.render(newText, True, (0,0,0))
-        
-    def update(self, t): # just updates the frame / object
-
-        print "last update     ", self._last_update
-        timePassed = t + self._last_update
-        print "time passed    ", timePassed
-        if (timePassed) > self._delay:
-            if self._frame < self.animations.get(self._current_anim)[0] or self._frame > self.animations.get(self._current_anim)[1]:
-              self._frame = self.animations.get(self._current_anim)[0]
-
-            self._frame += timePassed/self._delay
-            print "frame   ", self._frame
-
-            while self._frame >= self.animations.get(self._current_anim)[1]:
-              framesPast = self._frame - self.animations.get(self._current_anim)[1]
-              self._frame = framesPast - 1 + self.animations.get(self._current_anim)[0]
-
-            self.image = self._images[self._frame]
-            self._last_update = timePassed%self._delay
-        else:
-           
-           self._last_update = timePassed
