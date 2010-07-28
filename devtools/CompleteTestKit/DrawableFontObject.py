51bd37fa7cafa7235edbbcf88db4ca9aa888d123
davesilver <nibor123@gmail.com>
2010-07-28 13:23:57 -0400

    Testing Drawable Font Objects

--- devtools/CompleteTestKit/DrawableFontObject.py
+++ devtools/CompleteTestKit/DrawableFontObject.py
@@ -6,8 +6,8 @@ class DrawableFontObject(DrawableObject, pygame.sprite.Sprite):
     def __init__(self,text,font,fps = 10, x = 0, y = 0, xVelocity = 0, yVelocity = 0):
 
         self.font = font
-        self.textImage = font.render(text, True, (255,255,255))
-        self._image = self.textImage
+        self.textImage = font.render(text, 1, (255,255,255))
+        #self._image = self.textImage
         DrawableObject.__init__(self, [self.textImage], '', fps, x, y, xVelocity, yVelocity)
 
     def changeText(self, newtext):
